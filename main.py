6bc013b69f40fd4be1e8b164fc0f691a3e2ef30e
suhaib <suhaibvbm@gmail.com>
2023-11-05 16:39:35 +0530

    map loaded to screen

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import turtle
 screen = turtle.Screen()
 screen.title("U.S.State Game")
 
+# TODO: Adding shape image
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)  # available to tutle to use
+
 
 screen.exitonclick()
