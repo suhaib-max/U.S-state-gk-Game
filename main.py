8da2ea8e5c830ed80ebe28661321eeb45eaa8aaf
suhaib <suhaibvbm@gmail.com>
2023-11-05 16:53:41 +0530

    created input tab to input state_name

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ screen.addshape(image)
 
 turtle.shape(image)  # available to tutle to use
 
-
+answer_state = screen.textinput(title="Guess The State", prompt="what's another state 's name")
+print(answer_state)
 
 # replacing screen.exitonclick() to turtle.mainloop() to screen onn
 turtle.mainloop()
