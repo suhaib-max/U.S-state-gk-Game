3872852c9c5c8601faad7fc85783b4de804d10ed
suhaib <suhaibvbm@gmail.com>
2023-11-05 20:25:13 +0530

    completed the entering part and displaying

--- main.py
+++ main.py
@@ -13,15 +13,18 @@ turtle.shape(image)  # available to tutle to use
 data = pd.read_csv("50_states.csv")
 all_state = data.state.to_list()
 
-answer_state = screen.textinput(title="Guess The State", prompt="what's another state 's name")
-# replacing screen.exitonclick() to turtle.mainloop() to screen onn
+guessed_state = []
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)} The State", prompt="what's another state 's name").title()
+    # replacing screen.exitonclick() to turtle.mainloop() to screen onn
 
-if answer_state in all_state:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]  # this will pull out the row
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state.item())  # item will give first element in pandas series
+    if answer_state in all_state:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]  # this will pull out the row
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())  # item will give first element in pandas series
 
 turtle.mainloop()
